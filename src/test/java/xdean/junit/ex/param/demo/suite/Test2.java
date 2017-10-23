44cf53db2a914867e32001815b05c0e888ae6300
XDean <373216024@qq.com>
2017-10-23 15:45:46 +0800

    test rule

--- src/test/java/xdean/junit/ex/param/demo/suite/Test2.java
+++ src/test/java/xdean/junit/ex/param/demo/suite/Test2.java
@@ -1,9 +1,14 @@
 package xdean.junit.ex.param.demo.suite;
 
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class Test2 {
+
+  @Rule
+  public ExpectedException ee = ExpectedException.none();
 
   @Ignore
   @Test
@@ -13,5 +18,7 @@ public class Test2 {
 
   @Test
   public void other22() throws Exception {
+    ee.expect(IllegalArgumentException.class);
+    throw new IllegalArgumentException();
   }
 }
